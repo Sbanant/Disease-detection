b7f6cba1901ae7eeb28650b0e8fbc258caba9ada
Anant <111248158+Sbanant@users.noreply.github.com>
2023-07-23 05:50:49 +0530

    Update disease2.py

--- disease2.py
+++ disease2.py
@@ -2,18 +2,17 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-# Step 1: Load your dataset
-your_dataset_file = "C:/Users/anant/Downloads/archive/Symptom2Disease.csv" # Replace 'path_to_your_dataset.csv' with the actual file path
+#Load your dataset
+your_dataset_file = "path_to_your_dataset.csv" # Replace 'path_to_your_dataset.csv' with the actual file path
 dataset = pd.read_csv(your_dataset_file)
 # Step 2: Separate symptoms and diseases
-symptoms = dataset['text']
-diseases = dataset['label']
+symptoms = dataset['symptoms']#enter your column heading for symptoms
+diseases = dataset['diseases']#enter your column heading for diseases
 
-# Step 3: Vectorize symptoms using bag-of-words representation
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(symptoms)
 
-# Step 4: Train the Naive Bayes classifier
+
 classifier = MultinomialNB()
 classifier.fit(X, diseases)
 
@@ -26,7 +25,7 @@ def predict_disease(input_symptoms):
 
     return predicted_disease[0]
 
-# Example usage
+
 while True:
     user_input = input("Enter your symptoms (or 'exit' to quit): ")
     if user_input.lower() == 'exit':
